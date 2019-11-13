613988b05d2cae1a28b1a2f6745941ff75d256d1
Vesna Vucinic <vesna.vucinic68@gmail.com>
2019-11-13 12:41:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,15 +17,7 @@ require 'pry'
   def self.all
     @@songs
   end
-=begin
-  def self.reset_all
-    self.all.clear
-  end
 
-  def self.count
-    self.all.count
-  end
-=end
   def artist=(artist)
     @artist = artist
   end
