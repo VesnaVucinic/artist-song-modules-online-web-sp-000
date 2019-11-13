c7f048ec41efc0c0942ea16669706b537a4442ec
Vesna Vucinic <vesna.vucinic68@gmail.com>
2019-11-13 12:35:44 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,5 +1,5 @@
 module Findable 
   module ClassMethods 
-    def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
+    def find_by_name(name)
+    self.detect{|o| o.name == name}
   end
