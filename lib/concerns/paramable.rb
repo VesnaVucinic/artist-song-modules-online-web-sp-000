62fa450e1cf7572c9fabf2b0b0e5a50d0da9a782
Vesna Vucinic <vesna.vucinic68@gmail.com>
2019-11-13 12:51:40 +0000

    Automatically backed up by Learn

--- lib/concerns/paramable.rb
+++ lib/concerns/paramable.rb
@@ -4,4 +4,4 @@ module Paramable
     name.downcase.gsub(' ', '-')
   end
 end
-
+#It finds all instances of the matched string and replaces it with the new argument. The method takes two arguments. The first is the text you want to replace and the second is the new text.
